68236c9c6aa4b1f530c9e8db8bb54182389ff239
Bhavesh Patil <bhavesh.patil0325@gmail.com>
2024-05-10 23:13:30 +0530

    Location listener inside thread

--- app/src/main/java/com/assignment/devicelocationtracker/service/LocationUpdateService.java
+++ app/src/main/java/com/assignment/devicelocationtracker/service/LocationUpdateService.java
@@ -10,6 +10,7 @@ import android.location.LocationManager;
 import android.os.Binder;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.os.Looper;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
@@ -32,37 +33,44 @@ public class LocationUpdateService extends Service {
         return new LocationUpdateServiceBinder();
     }
 
-    @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d(TAG, "onStartCommand");
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            locationListener = new LocationListener() {
+            // Create a new thread for location updates
+            new Thread(new Runnable() {
                 @Override
-                public void onLocationChanged(Location location) {
-                    Log.d(TAG, "New location: " + location.getLatitude() + ", " + location.getLongitude());
-                    locationMutableLiveData.postValue(location);
-                }
+                public void run() {
+                    Looper.prepare();
+                    locationListener = new LocationListener() {
+                        @Override
+                        public void onLocationChanged(Location location) {
+                            Log.d(TAG, "New location: " + location.getLatitude() + ", " + location.getLongitude());
+                            locationMutableLiveData.postValue(location);
+                        }
 
-                @Override
-                public void onStatusChanged(String provider, int status, Bundle extras) {
-                    Log.d(TAG, "onStatusChanged: " + provider + ", status: " + status);
-                }
+                        @Override
+                        public void onStatusChanged(String provider, int status, Bundle extras) {
+                            Log.d(TAG, "onStatusChanged: " + provider + ", status: " + status);
+                        }
 
-                @Override
-                public void onProviderEnabled(String provider) {
-                    Log.d(TAG, "onProviderEnabled: " + provider);
-                }
+                        @Override
+                        public void onProviderEnabled(String provider) {
+                            Log.d(TAG, "onProviderEnabled: " + provider);
+                        }
 
-                @Override
-                public void onProviderDisabled(String provider) {
-                    Log.d(TAG, "onProviderDisabled: " + provider);
+                        @Override
+                        public void onProviderDisabled(String provider) {
+                            Log.d(TAG, "onProviderDisabled: " + provider);
+                        }
+                    };
+                    // Register the location listener with the GPS provider
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 0, locationListener);
+                    Looper.loop();
                 }
-            };
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 0, locationListener);
+            }).start();
         }
-
         return START_STICKY;
     }
 
